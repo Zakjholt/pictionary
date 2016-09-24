642934d5aa707b601eb6f8cb7c0089c6a2dbaf9c
zak <zak.j.holt@gmail.com>
2016-09-23 22:29:05 -0400

    functionality works

--- public/main.js
+++ public/main.js
@@ -2,7 +2,9 @@ var socket = io();
 
 var pictionary = function() {
     var canvas, context;
-
+    var drawing = false;
+    var drawer = false;
+    var mysteryWord;
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
@@ -15,25 +17,86 @@ var pictionary = function() {
     canvas[0].height = canvas[0].offsetHeight;
 
     canvas.on('mousemove', function(event) {
-        canvas.on('mousedown', function() {
-            drawing = true;
-        });
-        canvas.on('mouseup', function() {
-            drawing = false;
-        });
-        if (drawing) {
-            var offset = canvas.offset();
-            var position = {
-                x: event.pageX - offset.left,
-                y: event.pageY - offset.top
-            };
-            draw(position);
-            socket.emit('draw', position);
+        if (drawer) {
+            canvas.on('mousedown', function() {
+                drawing = true;
+            });
+            canvas.on('mouseup', function() {
+                drawing = false;
+            });
+            if (drawing) {
+                var offset = canvas.offset();
+                var position = {
+                    x: event.pageX - offset.left,
+                    y: event.pageY - offset.top
+                };
+                draw(position);
+                socket.emit('draw', position);
+            }
         }
+    });
+    socket.on('draw', draw);
+
+    ////Guess box
+    var guessBox;
+
+    var onKeyDown = function(event) {
+        if (event.keyCode != 13) { // Enter
+            return;
+        }
+        var guess = guessBox.val();
+
+        if (guess.toLowerCase() === mysteryWord) {
+            socket.emit('correct guess');
+            guessBox.val('');
+        } else {
+            socket.emit('guess', guess);
+            $('#guesses').prepend('<li>' + guess + '</li>');
+            guessBox.val('');
+        }
+    };
+
+    socket.on('guess', function(guess) {
+        $('#guesses').prepend('<li>' + guess + '</li>');
+    });
+
+    guessBox = $('#guess input');
+    guessBox.on('keydown', onKeyDown);
+
+    //Drawer Decision
+    socket.on('drawer', function(word) {
+        $('.waiting').hide();
+        $('#word-space').show();
+        $('#word').text(word);
+        drawer = true;
+        mysteryWord = word;
+    });
+
+    //Guesser Decision
+    socket.on('guesser', function(word) {
+        $('.waiting').hide();
+        $('#guess').show();
+        mysteryWord = word;
+    });
+
+    //End Game
+    socket.on('correct guess', function() {
+        drawer = false;
+        $('.magic-word').text(mysteryWord);
+        $('.end-game').show();
+        $('#word-space').hide();
+        $('#guess').hide();
+    });
+
+    //New Game
+    $('.new-game').click(function() {
+        socket.emit('new game');
+        $('.end-game').hide();
+        $('.waiting').show();
     });
 };
 
 $(document).ready(function() {
     pictionary();
-    socket.on('draw', draw(position));
+
 });
--- server.js
+++ server.js
@@ -12,9 +12,69 @@ server.listen(process.env.PORT || 8080, function() {
     console.log("Server is running at http://localhost:8080");
 });
 
-//Socket Listeners
+var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
 
-io.on('draw', function(position) {
-    console.log("Someone is drawing");
-    io.broadcast.emit('draw', position);
+
+var users = [];
+var word;
+var playersReady = 0;
+//Socket Listeners
+io.on('connection', function(socket) {
+    users.push(socket);
+    console.log('User connected. Current users: ' + users.length);
+    //User Selection Phase
+    function newGame() {
+        word = WORDS[Math.floor(Math.random() * WORDS.length)];
+        console.log('the word to guess is ' + word);
+
+        var drawer = users[Math.floor(Math.random() * users.length)];
+
+        drawer.emit('drawer', word);
+        drawer.broadcast.emit('guesser', word);
+    }
+
+    if (users.length > 1) {
+        newGame();
+    }
+
+    socket.on('new game', function() {
+        playersReady++;
+        if (playersReady === users.length) {
+            playersReady = 0;
+            newGame();
+        }
+    });
+
+    socket.on('draw', function(position) {
+        socket.broadcast.emit('draw', position);
+    });
+    socket.on('guess', function(guess) {
+        socket.broadcast.emit('guess', guess);
+        console.log('User made a guess: ' + guess);
+    });
+    socket.on('correct guess', function() {
+        console.log('Some one guessed the correct word: ' + word);
+        io.emit('correct guess');
+    });
+
+    socket.on('disconnect', function() {
+        var index = users.indexOf(socket);
+        users.splice(index, 1);
+        console.log('User disconnected. Current users: ' + users.length);
+        socket.broadcast.emit('user disconnected', users.length);
+    });
 });
